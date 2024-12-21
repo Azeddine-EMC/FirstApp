e7847bed44723da1fa6fc6df2689a4fe4937ddd2
Azeddine-EMC <a.bennehari.a@gmail.com>
2024-12-21 15:37:25 +0100

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -2,9 +2,12 @@ import streamlit as st
 
 st.title("EMC2 / Cloud Module")
 st.header("App pour test des AI app")
-st.video("https://youtu.be/YfZ0zk5Zzcw")
 #st.video("https://youtu.be/YfZ0zk5Zzcw")
 
 st.sidebar.image("https://seeklogo.com/images/E/ecole-hassania-des-travaux-publics-ehtp-logo-3D5770F217-seeklogo.com.png")
 st.sidebar.header("Master Cloud Computing")
 st.sidebar.selectbox('Select app type :', ['---choose application ---', 'Image Analysis', 'OCR', 'Thumbnail Image', 'Face Analysis'])
+choix=st.sidebar.selectbox('Select app type :', ['---choose application ---', 'Image Analysis', 'OCR', 'Thumbnail Image', 'Face Analysis'])
+
+if choix == 'Image Analysis':
+  image_file=st.file_uploader('load an image', type= ['png', 'jpg', 'jpeg'])
