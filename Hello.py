3e0998c7ad88271d169bace0802bd47156209a19
Azeddine-EMC <a.bennehari.a@gmail.com>
2024-12-21 15:38:10 +0100

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -6,7 +6,7 @@ st.header("App pour test des AI app")
 
 st.sidebar.image("https://seeklogo.com/images/E/ecole-hassania-des-travaux-publics-ehtp-logo-3D5770F217-seeklogo.com.png")
 st.sidebar.header("Master Cloud Computing")
-st.sidebar.selectbox('Select app type :', ['---choose application ---', 'Image Analysis', 'OCR', 'Thumbnail Image', 'Face Analysis'])
+
 choix=st.sidebar.selectbox('Select app type :', ['---choose application ---', 'Image Analysis', 'OCR', 'Thumbnail Image', 'Face Analysis'])
 
 if choix == 'Image Analysis':
